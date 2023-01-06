462e1a9967f934c9039f77fe10146a2cc42a44a8
shakilla1 <shakilshifat65@gmail.com>
2023-01-06 20:48:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.scss';
+import FileUpload from './FileUpload/FileUpload';
+import FileList from './FileList/FileList';
 
 function App() {
+  const [files, setFiles] = useState([])
+
+  const removeFile = (filename) => {
+    setFiles(files.filter(file => file.name !== filename))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Upload file</div>
+      <FileUpload files={files} setFiles={setFiles}
+        removeFile={removeFile} />
+      <FileList files={files} removeFile={removeFile} />
     </div>
   );
 }
